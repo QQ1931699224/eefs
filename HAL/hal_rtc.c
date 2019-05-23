0612347aa40bbd45640883151e9db056da150e55
吴晗帅 <1005091522@qq.com>
2019-05-23 11:10:03 +0800

    创建timer文件

---
+++ HAL/hal_rtc.c
@@ -0,0 +1,9 @@
+//
+//  hal_rtc.c
+//  eefs
+//
+//  Created by 吴晗帅 on 2019/5/23.
+//  Copyright © 2019 吴晗帅. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ service/timer/service_rtc_timing.c
@@ -0,0 +1,9 @@
+//
+//  service_rtc_timing.c
+//  eefs
+//
+//  Created by 吴晗帅 on 2019/5/23.
+//  Copyright © 2019 吴晗帅. All rights reserved.
+//
+
+#include "service_rtc_timing.h"
